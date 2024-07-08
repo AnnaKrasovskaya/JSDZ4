019f3b141598959403e8e74a6f280f8dbad0fa16
AnnaKrasovskaya <annkarry93@gmail.com>
2024-07-08 15:00:10 +0300

    dorabotki

--- app.js
+++ app.js
@@ -93,3 +93,176 @@
 //   return result;
 // };
 // console.log(ucFirst(string));
+
+// Задание 9
+// const str = "badWord";
+// chekSpam = (str) => {
+//   const newStr = str.toLowerCase();
+//   if (newStr.includes("badword") || newStr.includes("xxx")) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+// console.log(chekSpam(str));
+
+// Задание 10
+// const string = "привет, Женя";
+// func = (string) => {
+//   const newString = string.split("");
+//   return newString.reverse().join("");
+// };
+// console.log(func(string));
+
+// Задание 11
+// let stations = [
+//   "MAN675847583748sjt567654;Manchester Piccadilly",
+
+//   "GNF576746573fhdg4737dh4;Greenfield",
+
+//   "LIV5hg65hd737456236dch46dg4;Liverpool Lime Street",
+
+//   "SYB4f65hf75f736463;Stalybridge",
+
+//   "HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield",
+// ];
+
+// for (const element of stations) {
+//   const code = element.slice(0, 3);
+//   const symbol = element.indexOf(";");
+//   const readable = element.split(";")[1];
+//   console.log(code + ": " + readable);
+// }
+
+// Задание 12
+// let strings = [
+//   "кришна",
+//   "кришна",
+//   "харе",
+//   "харе",
+//   "харе",
+//   "харе",
+//   "кришна",
+//   "кришна",
+//   ":-O",
+// ];
+
+// const newString = [];
+// strings.forEach(function (element) {
+//   if (newString.indexOf(element) < 0) {
+//     newString.push(element);
+//   }
+// });
+// console.log(newString);
+
+// Задание 13
+
+// const catsJane = [4, 5, 3, 11, 6, 2, 4, 1, 5, 9];
+// const catsJulia = [2, 4, 5, 1, 13, 2, 15, 8, 3, 7];
+// const catsJane2 = [3, 5, 9, 14, 1, 2, 6, 8, 3, 10];
+// const catsJulia2 = [8, 2, 10, 1, 2, 5, 6, 3, 1, 4];
+// const verifyCats = (arr1, arr2) => {
+//   arr1.pop();
+//   arr1.shift();
+//   const newArr = arr1.concat(arr2);
+//   newArr.forEach(function (element, index) {
+//     if (element >= 2) {
+//       console.log(`Кошка №${index + 1} взрослая, ей ${element} лет`);
+//     } else {
+//       console.log(`Кошка №${index + 1} еще котенок`);
+//     }
+//   });
+// };
+// verifyCats(catsJane, catsJulia);
+// verifyCats(catsJane2, catsJulia2);
+
+//Задание 14
+
+// let arr = [5, 4, 3, 8, 0];
+// const filterFor = (arr, a) => {
+//   return arr.filter(function (item) {
+//     return item >= a;
+//   });
+// };
+// console.log(filterFor(arr, 5));
+
+// Задание 15
+
+// const string = ["yes", "hello", "no", "easycode", "what"];
+// const newString = (item) => {
+//   return item.filter(function (item) {
+//     return item.length > 3;
+//   });
+// };
+// console.log(newString(string));
+
+// Задание 16
+
+// const arr = [[14, 45], [1], ["a", "c", "d"]];
+// const sortedArr = arr.sort(function (a, b) {
+//   return a.length - b.length;
+// });
+// console.log(sortedArr);
+
+// Задание 17
+
+// const catAges = [7, 3, 2, 4, 1, 15, 8, 1, 9, 2];
+// const catAges2 = [1, 16, 12, 4, 5, 1, 3, 11, 7, 2];
+// const getAverageHumanAge = (arr) => {
+//   const ageHuman = arr.map(function (element) {
+//     if (element <= 2) {
+//       return element * 10;
+//     } else {
+//       return element * 7;
+//     }
+//   });
+//   const bigCats = ageHuman.filter(function (element) {
+//     return element > 18;
+//   });
+//   const summOfAges = bigCats.reduce(function (acc, element) {
+//     return acc + element;
+//   }, 0);
+//   const middleAge = summOfAges / bigCats.length;
+//   return middleAge;
+// };
+// console.log(getAverageHumanAge(catAges));
+// console.log(getAverageHumanAge(catAges2));
+
+//Math
+
+// const result = Math.round(5.4); //Классическое округление
+// console.log(result);
+
+// const result2 = Math.round(5.5);
+// console.log(result2);
+
+// const result3 = Math.round(5.6);
+// console.log(result3);
+
+// const result4 = Math.ceil(5.4); //Округление вверх до 6
+// console.log(result4);
+
+// const result5 = Math.ceil(5.5);
+// console.log(result5);
+
+// const result6 = Math.ceil(5.6);
+// console.log(result6);
+
+// const result7 = Math.floor(5.4); //Округление вниз до 5
+// console.log(result7);
+
+// const result8 = Math.floor(5.5);
+// console.log(result8);
+
+// const result9 = Math.floor(5.6);
+// console.log(result9);
+
+//Date
+
+// const date = new Date();
+// const options = { year: "numeric", month: "long", day: "numeric" };
+// const newDate = date.toLocaleDateString("ru-RU", options);
+// const hours = date.getHours();
+// const minutes = date.getMinutes();
+// console.log(`Сегодня ${newDate}`);
+// console.log(`${hours} часов ${minutes} минут`);
